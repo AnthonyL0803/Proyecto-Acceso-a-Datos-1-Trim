c26209087a5f0553c7c0efd4bc060c1ed8755c20
Anton Luo <22301690@live.uem.es>
2024-11-18 21:29:43 +0100

    Casi terminado
    Solo falta, gestión de errores.

--- Practica_Final - Primer Trimestre/src/AlmacenarPaises/DAOPaises.java
+++ Practica_Final - Primer Trimestre/src/AlmacenarPaises/DAOPaises.java
@@ -1,5 +1,73 @@
 package AlmacenarPaises;
 
-public class DAOPaises {
+import java.sql.*;
 
+public class DAOPaises {
+    private String url;
+    private String proyectPath;
+
+    public DAOPaises() {
+        this.proyectPath = System.getProperty("user.dir");
+        this.url = ("jdbc:h2:file:" + this.proyectPath + "\\Practica_Final - Primer Trimestre\\src\\Paises.db");
+    }
+
+    public void crearTablaPaises() throws SQLException {
+        try (Connection conexion = DriverManager.getConnection(this.url);
+             PreparedStatement sentencia = conexion.prepareStatement(
+                     "CREATE TABLE IF NOT EXISTS TABLAPAISES (NOMBRE VARCHAR(50) PRIMARY KEY, PRESIDENTE VARCHAR(50), PIB BIGINT, COEFICIENTEDEGINI DOUBLE)"
+             )) {
+            sentencia.execute();
+        }
+    }
+
+    public boolean existePais(Pais a) throws SQLException {
+        Connection conexion = DriverManager.getConnection(this.url);
+        PreparedStatement sentencia = conexion.prepareStatement("SELECT * FROM TABLAPAISES WHERE NOMBRE = ? AND PRESIDENTE = ? AND PIB = ? AND COEFICIENTEDEGINI = ?");
+        sentencia.setString(1, a.getNombre());                   // NOMBRE
+        sentencia.setString(2, a.getPresidente());               // PRESIDENTE
+        sentencia.setLong(3, a.getPIB());                        // PIB
+        sentencia.setDouble(4, a.getCoeficienteDeGini());        // COEFICIENTEDEGINI
+        ResultSet result = sentencia.executeQuery();
+        return result.next();
+    }
+
+    public void insertarPais(Pais a) throws SQLException {
+        Connection conexion = DriverManager.getConnection(this.url);
+        if (!existePais(a)) {
+            PreparedStatement sentencia = conexion.prepareStatement("INSERT INTO TABLAPAISES VALUES(?, ?, ?, ?)");
+            sentencia.setString(1, a.getNombre());                // NOMBRE
+            sentencia.setString(2, a.getPresidente());            //PRESIDENTE
+            sentencia.setLong(3, a.getPIB());                     // PIB
+            sentencia.setDouble(4, a.getCoeficienteDeGini());     // COEFICIENTEDEGINI
+            sentencia.execute();
+            conexion.close();
+        }
+    }
+
+    public void mostrarTabla() throws SQLException {
+        Connection conexion = DriverManager.getConnection(this.url);
+        PreparedStatement sentencia = conexion.prepareStatement("SELECT * FROM TABLAPAISES");
+        ResultSet resultado = sentencia.executeQuery();
+        while (resultado.next()) {
+            System.out.print(resultado.getString("NOMBRE") + " ");
+            System.out.print(resultado.getString("PRESIDENTE") + " ");
+            System.out.print(resultado.getLong("PIB") + " ");
+            System.out.print(resultado.getDouble("COEFICIENTEDEGINI") + " ");
+            System.out.println("Siguiente país: ");
+        }
+    }
+
+    public void incrementarPIB() throws SQLException {
+        Connection conexion = DriverManager.getConnection(this.url);
+        PreparedStatement sentencia = conexion.prepareStatement("UPDATE TABLAPAISES SET PIB = PIB + 10000000");
+        sentencia.execute();
+        conexion.close();
+    }
+
+    public void reduccionCoeficienteDeGini() throws SQLException {
+        Connection conexion = DriverManager.getConnection(this.url);
+        PreparedStatement sentencia = conexion.prepareStatement("UPDATE TABLAPAISES SET COEFICIENTEDEGINI = ((COEFICIENTEDEGINI/3) + (COEFICIENTEDEGINI/3) WHERE NOMBRE = 'El Salvador' OR NOMBRE = 'Honduras' OR NOMBRE = 'México'");
+        sentencia.execute();
+        conexion.close();
+    }
 }
--- Practica_Final - Primer Trimestre/src/AlmacenarPaises/Main.java
+++ Practica_Final - Primer Trimestre/src/AlmacenarPaises/Main.java
@@ -1,5 +1,7 @@
 package AlmacenarPaises;
 import java.io.*;
+import java.sql.SQLException;
+
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.security.NoTypePermission;
 import com.thoughtworks.xstream.security.NullPermission;
@@ -9,38 +11,32 @@ import com.thoughtworks.xstream.security.PrimitiveTypePermission;
 Introduce en el Proyect structure > Module > jar = kxml y el xstream
  */
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
         XStream xstream = new XStream();
         FileInputStream fis = new FileInputStream("Paises.xml");
+        ListaPaises listaPaises = new ListaPaises();
+        DAOPaises daoPaises = new DAOPaises();
 
-        XStream.setupDefaultSecurity(xstream); // Configuración básica
-        xstream.addPermission(NoTypePermission.NONE); // No permitir nada por defecto
-        xstream.addPermission(NullPermission.NULL); // Permitir nulos
-        xstream.addPermission(PrimitiveTypePermission.PRIMITIVES); // Permitir tipos primitivos
-        xstream.allowTypesByRegExp(new String[] { ".*" });
-
-
-        xstream.alias("Pais", Pais.class);
-        xstream.alias("ListaPaises", ListaPaises.class);
-
-        xstream.useAttributeFor(Pais.class, "Nombre");
-        xstream.useAttributeFor(Pais.class, "Presidente");
-        xstream.useAttributeFor(Pais.class, "PIB");
-        xstream.useAttributeFor(Pais.class, "CoeficienteDeGini");
-        xstream.addImplicitCollection(ListaPaises.class, "paises");
-
-        xstream.alias("Paises", ListaPaises.class);
-        xstream.alias("Pais", Pais.class);
-
-        ListaPaises listaPaises = (ListaPaises) xstream.fromXML(fis);
-
+        //En este paso leemos el archivo xml y almacenamos los objetos en la lista
+        LeerXMLXstream leerXMLXstream = new LeerXMLXstream();
+        listaPaises = leerXMLXstream.leerXML(xstream, fis, listaPaises);
+        //Comprobamos que los objetos se han almacenado correctamente
         for (Pais pais : listaPaises.getPais()) {
             System.out.println(pais);
         }
-
         //En este paso creamos el fichero .dat
         PaisesBinario paisesBinario = new PaisesBinario();
         paisesBinario.crearFicheroDat(listaPaises);
+
+        //En este paso creamos la tabla insertamos los paises y posteriormente mostramos todos los datos
+        daoPaises.crearTablaPaises();
+        for (Pais pais : listaPaises.getPais()){
+            daoPaises.insertarPais(pais);
+        }
+        System.out.println("\n");
+        daoPaises.mostrarTabla();
+        System.out.println("\n");
+        daoPaises.incrementarPIB();
 
     }
 }
